0fc77a4607a6603001366f0bffc14fd4cdfb2bbb
waarrdii <waarrdii@gmail.com>
2024-12-04 13:23:40 +0700

    #1b. Top navbar ke - 2 , next data components

--- resources/js/components/TopNavbar.vue
+++ resources/js/components/TopNavbar.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="bg-gray-50 h-16 flex flex-row gap-3 relative items-center">
-        <div v-for="item in (Object.keys(navBar))" :key="item" class="flex flex-col border gap-4">
+    <div class="bg-gray-50 h-16 flex flex-row gap-3 relative items-start">
+        <div v-for="item in (Object.keys(navBar))" :key="item" class="flex flex-col border gap-4 mt-2">
             <RouterLink  :to="{ name: navBar[item].routeName }">
                 {{ item }}
             </RouterLink>
-            <div class="absolute bottom-0 border left-0">
-                <span>second navbar</span> <span>second navbar</span>
+            <div 
+            v-if="router.currentRoute.value.name === navBar[item].routeName"
+            class="absolute bottom-0 border left-0">
+                {{ navBar[item].title }}
             </div>
         </div>
         
@@ -17,6 +19,7 @@
 import { computed, onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
 import { useStore } from 'vuex';
+import router from '../router';
 
 const Store = useStore();
 
