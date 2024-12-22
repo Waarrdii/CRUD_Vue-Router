0982dc3aa962f07d107258ac2f788d669b48ef25
wardi <waarrdii@gmail.com>
2024-12-22 23:34:44 +0700

    sidebar ok

--- resources/js/components/CardMenu.vue
+++ resources/js/components/CardMenu.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="absolute grid grid-cols-3 left-48 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 gap-2">
+        class="absolute left-full top-0 grid grid-cols-3 min-w-max p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 gap-2">
         <div class="border border-gray-200 rounded-lg p-2 hover:bg-gray-100 dark:border-gray-700 flex flex-col gap-1 text-center"
         v-for="item in items" :key="item">
             <i :class="`mdi ${item.icon}`" class="text-2xl text-gray-500"></i>
--- resources/js/components/SidebarNav.vue
+++ resources/js/components/SidebarNav.vue
@@ -1,24 +1,52 @@
 <template>
-    <div class="bg-slate-900">
-        <ul class="h-screen border-r border-gray-50  w-48 flex flex-col flex-start gap-3 px-6 pt-16">
-        <li class="flex gap-1 cursor-pointer select-none items-center text-gray-100" 
-            v-for="(item, index) in sidebarData" :key="item.name"
-            @click="openMenu(index)">
-            <i :class="`mdi ${item.icon}`" class="text-3xl text-gray-100 border border-transparent hover:border hover:border-rose-500 hover:bg-rose-500 rounded py-1 px-2"></i>
-            <span class="text-sm">{{ item.title }}</span>
-            <CardMenu :items="item.subMenu" :class="{ 'hidden': !item.isOpen }"/>
-        </li>
-    </ul>
-    </div>
+    <aside id="sidebar-menu"
+        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar">
+        <div class="h-full px-3 py-4 overflow-y-auto bg-blue-900  dark:bg-gray-800">
+            <ul class="space-y-2 font-medium">
+                <li>
+                    <a href="#"
+                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                            viewBox="0 0 22 21">
+                            <path
+                                d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                            <path
+                                d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                        </svg>
+                        <span class="ms-3">Dashboard</span>
+                    </a>
+                </li>
+                <li v-for="(item, index) in sidebarData" :key="item.name">
+                    <button @click="openMenu(index)" type="button"
+                        :class="{ 'bg-rose-500 bg-opacity-500': item.isOpen }"
+                        class="flex items-center w-full p-2 text-base text-gray-100 transition duration-75 rounded-lg group hover:bg-rose-500 dark:text-white dark:hover:bg-gray-700"
+                        aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                        <i :class="`mdi ${item.icon}`" class="text-3xl "></i>
+                        <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ item.title }}</span>
+                    </button>
+                    <ul id="subMenu" class=" bg-rose-50 text-gray-800 rounded-lg" v-if="item.isOpen">
+                        <li v-for="component in item.subMenu" :key="component">
+                            <div
+                                class="flex items-center w-full py-1 text-gray-900 transition duration-75 rounded-lg px-4 group cursor-pointer space-x-2 hover:bg-rose-100 dark:text-white dark:hover:bg-gray-700">
+                                <i :class="`mdi ${component.icon}`" class="text-xl "></i>
+                                <span class="text-sm capitalize">{{ component.subTitle }}</span>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
 
-
-
+            </ul>
+        </div>
+    </aside>
 </template>
 
 <script setup>
 import { onMounted, reactive } from 'vue';
 import { useStore } from 'vuex';
-import CardMenu from './CardMenu.vue';
+
+
 
 const Store = useStore();
 const sidebarData = reactive([]);
@@ -68,6 +96,8 @@ const getActiveRoute = (item) => {
     }
     return { name: item.toLowerCase() };
 }
+
+
 </script>
 
 <style lang="scss" scoped></style>
